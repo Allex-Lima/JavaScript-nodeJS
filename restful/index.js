872e7446ff1c15a167d772a58e6fadcd553c0e14
Alexandro <alexlmangabeira@gmail.com>
2022-03-24 19:39:53 -0300

    Start Project

---
+++ restful/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routesIndex = require('./routes/index');
+const routesUsers = require('./routes/users');
+let app = express();
+
+
+app.use('/',routesIndex);
+app.use('/users',routesUsers);
+
+app.listen(3000, '127.0.0.1', () => {
+    console.log('Server Run');
+})
---
+++ restful/routes/users.js
@@ -0,0 +1,25 @@
+const express = require('express');
+routes = express.Router();
+
+routes.get('/', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ 
+        users: [{
+            name: 'Alex',
+            email: 'alex@gmail.com',
+            id: 1
+        }]
+    });
+});
+
+routes.get('/admin', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ 
+        users: []
+    });
+});
+
+
+module.exports = routes;
