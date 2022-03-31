f81e68372ff940a72e521232f49719efddb8e40e
Alexandro <alexlmangabeira@gmail.com>
2022-03-30 21:37:57 -0300

    method get and post, with database NeDB

--- restful/routes/users.js
+++ restful/routes/users.js
@@ -1,21 +1,38 @@
+const NeDB = require('nedb');
+const dataBase = new NeDB({
+    filename: 'users.db',
+    autoload: true
+});
+
+
 module.exports = (app) => {
     app.get('/users', (req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({ 
-            users: [{
-                name: 'Bárbara',
-                email: 'alex@gmail.com',
-                id: 1
-            }]
-        });
+        dataBase.find({}).sort({name: 1}).exec((err, users) => {
+            if (err) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                });
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({
+                    users
+                });
+            }
+        })
     });
     
-    app.get('/users/admin', (req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({ 
-            users: ['Ola']
-        });
+    app.post('/users', (req, res) => {
+        dataBase.insert(req.body, (err, user) => {
+            if (err) {
+                console.log(`error: ${err} Bad Request`);
+                res.status(400).json({
+                    error: err
+                });
+            } else {
+                res.status(200).json(user);
+            }
+        })
     });
 };
